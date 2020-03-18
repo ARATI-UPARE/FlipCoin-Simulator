e8186bf3bd12a3ba0ccc888b6b9151af5a9f41f4
ARATI-UPARE <aratiupare@gmail.com>
2020-03-18 15:42:14 +0530

    ADDED: Singleton combination of Head and Tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,15 +1,33 @@
 #! /bin/bash -x
 
-# WAP to dislpay Head or Tail when flip a coin
+# WAP to dislpay Percentage of Head or Tail when flipping a coin in loop and store in dictionary
 
-coin=$((RANDOM%2))
+declare -A dict
 
-if [[ $coin -eq 1 ]]
-then
-		echo head
-else
-		echo tail
-fi
+flip=0
+head=0
+tail=0
+
+# For singleton combination.
+while [[ $flip -lt 10 ]]
+do
+		coin=$((RANDOM%2))
+		if [[ $coin -eq 1 ]]
+		then
+				(( head++ ))
+		else
+				(( tail++ ))
+		fi
+		(( flip++ ))
+done
+
+perHead=`echo "scale=2;($head / 10 ) * 100"|bc`
+perTail=`echo "scale=2;($tail / 10 ) * 100"|bc`
+dict=( [Head]=$head [Tail]=$tail )
+echo "${!dict[@]}=${dict[@]}"
+
+echo "Percentage of Head is : $perHead"
+echo "Percentage of Tail is : $perTail"
 
 
 
